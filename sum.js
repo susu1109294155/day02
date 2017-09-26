448b2dee8bc4230c7178510b2f4e92ff450bcf46
suao <1109294155@qq.com>
2017-09-26 17:52:44 +0800

    sss

--- sum.js
+++ sum.js
@@ -1,6 +1,6 @@
-var d = function () {
+function jia() {
     var a = parseInt(document.getElementById("aid").innerHTML);
     var b = parseInt(document.getElementById("bid").innerHTML);
     var c = a + b + "";
-    document.write(c);
-};
+    document.getElementById("sumid").innerHTML = c;
+}
--- sum.ts
+++ sum.ts
@@ -1,8 +1,8 @@
 
-const d = () => {
+function jia() {
 	const a: number = parseInt(document.getElementById("aid").innerHTML);
 	const b: number = parseInt(document.getElementById("bid").innerHTML);
 	const c: string = a + b + "";
-	document.write(c);
+	document.getElementById("sumid").innerHTML = c;
 }
 
