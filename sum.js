7ee1c5370d4c05d2320d4d707a4d724671c4487e
suao <1109294155@qq.com>
2017-09-26 16:55:58 +0800

    sss

---
+++ sum.js
@@ -0,0 +1,7 @@
+function jia() {
+    var a = 2;
+    var b = 2;
+    var c = a + b;
+    var d = "" + c;
+    document.getElementById("sumid").innerHTML = d;
+}
---
+++ sum.ts
@@ -0,0 +1,9 @@
+function jia():any{
+    var a:number = 2;
+    var b:number = 2;
+    var c:number=a+b;
+    var d:string = ""+c;
+
+    document.getElementById("sumid").innerHTML=d;
+   
+}
